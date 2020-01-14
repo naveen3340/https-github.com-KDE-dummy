94e20fdcbc7403554f6fa5306aa25526724072dd
Reynaldo Salazar <reysalazar1213@gmail.com>
2020-01-14 03:25:45 -0600

    {"stats":[{"file":"Example_java","add":1,"del":1}],"wait":20, "err":false}

--- Example.java
+++ Example.java
@@ -4,7 +4,10 @@ public class Example {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
 
-    System.out.print("Input first number")
+    System.out.print("Input first number: ");
+    int x = in.nextInt();
+    System.out.print("Input first number: ");
+    System.out.print("Input first number: ");
   }
 
 }
