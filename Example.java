2dee995ff1b3828cdaa150b6338388958dbedc74
Reynaldo Salazar <reysalazar1213@gmail.com>
2020-01-14 10:13:16 -0600

    {"stats":[{"file":"_tempCodeRunnerFile.txt","add":14,"del":0},{"file":"Example_java","add":1,"del":1}],"wait":0, "err":false}

--- Example.java
+++ Example.java
@@ -20,7 +20,7 @@ public class Example {
       System.out.println("Neither all are equal or different");
     }
     else {
-      System.out.println("All numbers are different");
+      System.out.println("All numbers are different")
     }
   }
 
