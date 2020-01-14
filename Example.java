fff538d950d988682425d2f1bd433545b7e0f83f
Reynaldo Salazar <reysalazar1213@gmail.com>
2020-01-14 03:30:25 -0600

    {"stats":[{"file":"Example_java","add":2,"del":0}],"wait":20, "err":false}

--- Example.java
+++ Example.java
@@ -6,8 +6,10 @@ public class Example {
 
     System.out.print("Input first number: ");
     int x = in.nextInt();
+
     System.out.print("Input second number: ");
     int y = in.nextInt();
+    
     System.out.print("Input third number: ");
     int z = in.nextInt();
 
