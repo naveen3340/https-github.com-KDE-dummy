8da4d9aa0d18ce7ab1c89638ff7c5ad8423a55b2
Reynaldo Salazar <reysalazar1213@gmail.com>
2020-01-14 00:55:54 -0600

    {"stats":[{"file":"src-ex_java","add":5,"del":2}],"wait":20, "err":false}

--- src/ex.java
+++ src/ex.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class Ex {
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-
     int x = 1;
+    int y = 2;
+    int z = 3;
+
     System.out.print("Input first number: ");
     
     System.out.print("Input second number: ");
-    int y = 2;
+    
     System.out.print("Input third number: ");
-    int z = 3;
+    
 
     if(x == y && x == z) {
       System.out.println("All numbers are equal");
