3d2b33baf4f994cfa54eefd1f4acb58fd11202fa
Reynaldo Salazar <reysalazar1213@gmail.com>
2020-01-14 03:25:05 -0600

    {"stats":[{"file":"Example_java","add":3,"del":1}],"wait":20, "err":false}

--- Example.java
+++ Example.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Example {
-  public static void main(String[] args)
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    System.out.print()
+  }
 
 }
