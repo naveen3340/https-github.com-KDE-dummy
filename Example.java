fe07698675d6f1c485a934f5371297061b38d7f4
Reynaldo Salazar <reysalazar1213@gmail.com>
2020-01-31 09:09:11 -0600

    {"stats":[{"file":"Example_java","add":3,"del":3}],"wait":30,"run":false,"err":false}

--- Example.java
+++ Example.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Example {
   public static void main(String[] args)
     {
-      Scanner in = new Scanner(System.in);
+      //Scanner in = new Scanner(System.in);
       
       System.out.print("Input first number: ");
-      int x = in.nextInt();
+      //int x = in.nextInt();
+      int x=1
       System.out.print("Input second number: ");
-      int y = in.nextInt();
+      //int y = in.nextInt();
+      int y = 2
       System.out.print("Input third number: ");
-      int z = in.nextInt();
+      //int z = in.nextInt();
       
       if (x == y && x == z)
       {
